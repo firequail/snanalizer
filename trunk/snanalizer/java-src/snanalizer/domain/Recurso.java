644c660dd3e4bf329dff823b6f9d8b42d22b095d
nicolasfont <nicolasfont@c628e0b4-e753-0410-9938-6323b9659747>
2008-09-08 02:13:04 +0000

    sacando cosas de mas en el xml de los nodos y relaciones

--- trunk/snanalizer/java-src/snanalizer/domain/Recurso.java
+++ trunk/snanalizer/java-src/snanalizer/domain/Recurso.java
@@ -37,7 +37,7 @@ public class Recurso extends DomainEntity {
 	}
 	
 	public String toXml() {
-		//TODO: usar StringBuilder, pensar si no se puede hacer esto en flex, sacar colores y demas a styles
-		return "<Node id=\"" + getId() + "\" name=\"" + getNombre() + " " + getApellido() + "\" desc=\"Nodo 1\" nodeColor=\"0x333333\" nodeSize=\"32\" nodeClass=\"earth\" nodeIcon=\"center\" x=\"0\" y=\"0\" />";
+		//TODO: usar StringBuilder
+		return "<Node id=\"" + getId() + "\" name=\"" + getNombre() + " " + getApellido() + "\"/>";
 	}
 }
